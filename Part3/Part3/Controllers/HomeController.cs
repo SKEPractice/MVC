71359b927cf6ee5f14753880e40781f71613e2e5
uges.ske <uges.ske@gmail.com>
2018-04-22 15:31:33 +0545

    Completed Part #

---
+++ Part3/Part3/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Part3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "This is my first MVC Application";
+        }
+        public string GetDetails()
+        {
+            return "Get details Invoked";
+        }
+    }
+}
