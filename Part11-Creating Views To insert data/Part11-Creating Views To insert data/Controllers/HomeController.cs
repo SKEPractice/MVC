504baffd013be07a01b0a78fdb246a7ae08bd337
uges.ske <uges.ske@gmail.com>
2018-04-25 13:27:11 +0545

    Part12-Creating views to insert data completed

---
+++ Part11-Creating Views To insert data/Part11-Creating Views To insert data/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Part11_Creating_Views_To_insert_data.Models;
+namespace Part11_Creating_Views_To_insert_data.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeContext employeeContext = new EmployeeContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Employee> employeeList = employeeContext.Employees.ToList();
+            return View(employeeList);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Part11-Creating Views To insert data/Part11-Creating Views To insert data/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Part11_Creating_Views_To_insert_data.Models
+{
+    [Table("tblDepartment")]
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+        public string Name { get; set; }
+    }
+}
